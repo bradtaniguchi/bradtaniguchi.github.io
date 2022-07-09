ff11b9d1d0d921accd92a2f578996ed5ad57db0b
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-07-09 16:50:36 -0700

    Add basic layout of main home page

--- apps/internal-client/pages/index.tsx
+++ apps/internal-client/pages/index.tsx
@@ -1,5 +1,66 @@
+import { Box } from '@primer/react';
+
 export function Index() {
-  return <>base</>;
+  return (
+    <Box display="grid" gridGap={3} gridTemplateColumns="1fr 1fr 1fr">
+      <Box
+        borderColor="border.default"
+        borderWidth={1}
+        borderStyle="solid"
+        p={3}
+      >
+        <aside>Profile info goes here</aside>
+      </Box>
+
+      <Box gridColumn="span 2">
+        <Box gridColumn="span 2">
+          <Box display="grid" gridGap={3} gridTemplateColumns="1fr 1fr">
+            <Box
+              borderColor="border.default"
+              borderWidth={1}
+              borderStyle="solid"
+              p={3}
+              gridColumn="span 2"
+            >
+              README goes here
+            </Box>
+            <Box
+              borderColor="border.default"
+              borderWidth={1}
+              borderStyle="solid"
+              p={3}
+            >
+              Projects
+            </Box>
+            <Box
+              borderColor="border.default"
+              borderWidth={1}
+              borderStyle="solid"
+              p={3}
+            >
+              Snippets
+            </Box>
+            <Box
+              borderColor="border.default"
+              borderWidth={1}
+              borderStyle="solid"
+              p={3}
+            >
+              Blog
+            </Box>
+            <Box
+              borderColor="border.default"
+              borderWidth={1}
+              borderStyle="solid"
+              p={3}
+            >
+              About
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default Index;
