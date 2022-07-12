e507b8386e5c623b7b45d861716c4650ba53159f
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-07-11 20:39:18 -0700

    Add markdown loading

--- apps/internal-client/pages/index.tsx
+++ apps/internal-client/pages/index.tsx
@@ -1,5 +1,17 @@
-export function Index() {
-  return <>base</>;
+import { getMarkdown } from '../utils/get-markdown';
+
+export function Index(props: { content: string }) {
+  console.log('props', props);
+  return <div dangerouslySetInnerHTML={{ __html: props.content }}></div>;
 }
 
 export default Index;
+
+export async function getStaticProps() {
+  const content = await getMarkdown('README.md');
+  return {
+    props: {
+      content,
+    },
+  };
+}
