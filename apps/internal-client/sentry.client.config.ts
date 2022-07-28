8834ab5a7c73dccd48adafd23eeebd4e94dbe8e9
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-07-28 16:32:11 -0700

    Add sentry config

---
+++ apps/internal-client/sentry.client.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn:
+    SENTRY_DSN ||
+    'https://dcbb746651864c8dad430f32869bb4ea@o300628.ingest.sentry.io/6609584',
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
