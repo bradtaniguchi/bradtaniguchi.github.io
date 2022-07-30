2c6655f87314e988c4e844e024d480a83ef4f034
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-07-30 15:40:36 -0700

    Update public activity

--- apps/internal-client/components/activity/github-public-activity.tsx
+++ apps/internal-client/components/activity/github-public-activity.tsx
@@ -1,4 +1,5 @@
 import { MarkGithubIcon } from '@primer/octicons-react';
+import Link from 'next/link';
 import { Box, StyledOcticon, Timeline } from '@primer/react';
 import { GithubPublicActivity as GithubPublicActivityType } from '../../models/github-public-activity';
 import { memo } from 'react';
@@ -96,9 +97,35 @@ export const GithubPublicActivity = memo(function GithubPublicActivity(
               );
             case 'PushEvent':
               return (
-                <Box sx={{ display: 'inline' }}>
+                <Box
+                  sx={{ display: 'inline' }}
+                  title={JSON.stringify(props.activity, null, 2)}
+                >
                   <GithubPublicActivityLogin activity={props.activity} />
-                  pushed to{' '}
+                  pushed{' '}
+                  {(() => {
+                    type PayloadWithCommits = {
+                      commits: Array<{
+                        // this is only a partial
+                        url: string;
+                      }>;
+                    };
+                    const hasMultiple =
+                      (props.activity.payload as PayloadWithCommits).commits
+                        ?.length > 1;
+
+                    return (
+                      <Link
+                        href={
+                          (props.activity.payload as PayloadWithCommits)
+                            .commits[0].url
+                        }
+                      >
+                        <a>{hasMultiple ? 'commits ' : 'a commit '}</a>
+                      </Link>
+                    );
+                  })()}
+                  to {''}
                   <GithubPublicActivityRepo activity={props.activity} />
                   <GithubPublicActivityTime activity={props.activity} />
                 </Box>
@@ -107,7 +134,40 @@ export const GithubPublicActivity = memo(function GithubPublicActivity(
               return (
                 <Box sx={{ display: 'inline' }}>
                   <GithubPublicActivityLogin activity={props.activity} />
-                  pushed to{' '}
+                  created pull request for{' '}
+                  <GithubPublicActivityRepo activity={props.activity} />
+                  <GithubPublicActivityTime activity={props.activity} />
+                </Box>
+              );
+            case 'IssuesEvent':
+              return (
+                <Box sx={{ display: 'inline' }}>
+                  <GithubPublicActivityLogin activity={props.activity} />
+                  created issue{' '}
+                  <Link
+                    href={props.activity.payload.issue.url}
+                    title={props.activity.payload.issue.title}
+                  >
+                    <a>{`#${props.activity.payload.issue.number}`}</a>
+                  </Link>{' '}
+                  for <GithubPublicActivityRepo activity={props.activity} />
+                  <GithubPublicActivityTime activity={props.activity} />
+                </Box>
+              );
+            case 'WatchEvent':
+              if (props.activity.payload.action === 'started')
+                return (
+                  <Box sx={{ display: 'inline' }}>
+                    <GithubPublicActivityLogin activity={props.activity} />
+                    stared repo{' '}
+                    <GithubPublicActivityRepo activity={props.activity} />
+                    <GithubPublicActivityTime activity={props.activity} />
+                  </Box>
+                );
+              return (
+                <Box sx={{ display: 'inline' }}>
+                  <GithubPublicActivityLogin activity={props.activity} />
+                  un-stared repo{' '}
                   <GithubPublicActivityRepo activity={props.activity} />
                   <GithubPublicActivityTime activity={props.activity} />
                 </Box>
@@ -115,7 +175,12 @@ export const GithubPublicActivity = memo(function GithubPublicActivity(
             default:
               // TODO: add types
               return (
-                <Box sx={{ display: 'inline' }}>Unknown activity given</Box>
+                <Box
+                  sx={{ display: 'inline' }}
+                  title={JSON.stringify(props.activity, null, 2)}
+                >
+                  Unknown activity given
+                </Box>
               );
           }
         })()}
