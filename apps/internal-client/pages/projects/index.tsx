fe66711749f2daa1e3464cd8f0c276b2827998ad
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-08-06 11:54:43 -0700

    Add Card.Row component

--- apps/internal-client/pages/projects/index.tsx
+++ apps/internal-client/pages/projects/index.tsx
@@ -1,3 +1,31 @@
+import { Card } from '../../components/core/card';
+
+export interface ProjectsProps {
+  /**
+   * The list of projects to display.
+   */
+  projects: Array<unknown>;
+}
+
 export default function Projects() {
-  return <div>List of projects...</div>;
+  return (
+    <Card>
+      <Card.Header>Projects</Card.Header>
+      <Card.Body p={0}>
+        <Card.Row p={3}>testRow</Card.Row>
+        <Card.Row p={3}>testRow</Card.Row>
+        <Card.Row p={3}>testRow</Card.Row>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export async function getStaticProps(): Promise<{
+  props: ProjectsProps;
+}> {
+  return {
+    props: {
+      projects: [],
+    },
+  };
 }
