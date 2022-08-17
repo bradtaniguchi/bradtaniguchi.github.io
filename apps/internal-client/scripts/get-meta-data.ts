88490962a8ea907597a47042fcf500f1916242f0
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-08-16 17:47:25 -0700

    Add meta-data loading for path
    - will verify all files in path

--- apps/internal-client/scripts/get-meta-data.ts
+++ apps/internal-client/scripts/get-meta-data.ts
@@ -1,7 +1,12 @@
+import { stat } from 'fs-extra';
 import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 const argv = yargs(hideBin(process.argv)).argv;
-import { getProjectMetaData } from '../utils/get-project-meta-data';
+import {
+  getProjectMetaData,
+  getProjectsMetaData,
+} from '../utils/get-project-meta-data';
+import { getMarkdownFiles } from '../utils/get-markdown-files';
 
 (async () => {
   try {
@@ -10,11 +15,19 @@ import { getProjectMetaData } from '../utils/get-project-meta-data';
     if (!path) throw new Error('Missing --path arg, please path is');
     if (typeof path !== 'string') throw new Error('--path is not a string');
 
-    console.log('');
+    const pathStat = await stat(path);
 
-    const metaData = await getProjectMetaData(path);
+    if (!pathStat.isDirectory()) {
+      const metaData = await getProjectMetaData(path);
 
-    console.log(metaData);
+      console.log(metaData);
+      return;
+    }
+
+    const projectPaths = await getMarkdownFiles(path);
+    const projectsMetaData = await getProjectsMetaData(projectPaths);
+
+    console.log(projectsMetaData);
 
     process.exit(0);
   } catch (err) {
