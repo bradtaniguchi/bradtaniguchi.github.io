fe17ad072652d6bbdeae7484c11cfab29253ae6d
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-08-17 21:24:33 -0700

    start adding logic for singular load
    [skip ci]

--- apps/internal-client/pages/projects/[name].tsx
+++ apps/internal-client/pages/projects/[name].tsx
@@ -1,8 +1,25 @@
+import { GetStaticPropsContext } from 'next';
 import { useRouter } from 'next/router';
+import { StaticProject } from '../../models/project';
 
-export default function Project() {
+export interface ProjectProps {
+  project: StaticProject;
+}
+
+export default function Project(props: ProjectProps) {
   const router = useRouter();
   const { name } = router.query;
 
   return <p>Project: {name}</p>;
+}
+
+export async function getStaticProps({
+  params,
+}: GetStaticPropsContext): Promise<ProjectProps> {
+  const { name } = params;
+  // TODO: get projects by name
+  const project: StaticProject = {} as unknown as StaticProject;
+  return {
+    project,
+  };
 }
