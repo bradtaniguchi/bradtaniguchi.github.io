6438da2477ea4aa858c1968800601554c5098123
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-09-04 10:52:10 -0700

    fix: remove front-matter rendering from posts
    - used workaround
    
    Close #77

--- apps/internal-client/utils/get-markdown.ts
+++ apps/internal-client/utils/get-markdown.ts
@@ -5,10 +5,13 @@ import { marked } from 'marked';
 
 /**
  * Returns the parsed markdown as an HTML string from the given path.
+ * Automatically removes front-matter text
  */
 export const getMarkdown = async (path: string) => {
   const rawFileContent = await readFile(path);
-  const unsafeMarkdown = marked.parse(rawFileContent.toString());
+  const unsafeMarkdown = marked.parse(
+    rawFileContent.toString().replace(/^---$.*^---$/ms, '')
+  );
   const DOMPurify = createDOMPurify(new JSDOM().window as unknown as Window);
   return DOMPurify.sanitize(unsafeMarkdown);
 };
