d172eecea6a3fe948f0ce9735664969f019c8109
Brad <bradtaniguchi@gmail.com>
2022-11-10 05:05:46 +0000

    Update DNS strings
    - moved to env

--- apps/internal-client/sentry.server.config.ts
+++ apps/internal-client/sentry.server.config.ts
@@ -7,9 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    'https://dcbb746651864c8dad430f32869bb4ea@o300628.ingest.sentry.io/6609584',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
