4140a29492bd64f05cd8602d8a903796afe39f25
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-12-09 15:29:24 -0800

    chore: toggle production source-maps

--- apps/internal-client/next.config.js
+++ apps/internal-client/next.config.js
@@ -10,6 +10,7 @@ const withNx = require('@nrwl/next/plugins/with-nx');
  **/
 const nextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
