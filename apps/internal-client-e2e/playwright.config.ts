a6d15af8fa3f003191841e94b72745489f46966f
Brad Taniguchi <bradtaniguchi@gmail.com>
2022-12-10 21:50:46 -0800

    chore: start adding pipeline
    - need to stop however, as the test script doesn't currently work
    correctly. Will need to write a custom CI script to execute the e2e
    tests locally.
    
    - will update the pipeline with a check against production instead.

--- apps/internal-client-e2e/playwright.config.ts
+++ apps/internal-client-e2e/playwright.config.ts
@@ -8,4 +8,11 @@ const config: PlaywrightTestConfig = {
   timeout: 5_000,
 };
 
+if (config.use?.baseURL === 'local') {
+  // special value, will target a pre-existing build of
+  // internal-client running on a http-server locally
+
+  config.use.baseURL = `http://127.0.0.1:8080`;
+}
+
 export default config;
