78f145c0b3223908ff47c8daa12b423e9900b8ae
Brad Taniguchi <bradtaniguchi@gmail.com>
2023-05-08 10:47:54 -0700

    chore: ran migrations

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,9 @@
 module.exports = {
-  stories: [],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
-    'storybook-dark-mode',
-  ],
-  features: { modernInlineRender: true, buildStoriesJson: true },
+  addons: ['@storybook/addon-a11y', 'storybook-dark-mode'],
+  features: {
+    modernInlineRender: true,
+    buildStoriesJson: true
+  }
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
@@ -13,4 +11,13 @@ module.exports = {
   //   // Return the altered config
   //   return config;
   // },
+  ,
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
