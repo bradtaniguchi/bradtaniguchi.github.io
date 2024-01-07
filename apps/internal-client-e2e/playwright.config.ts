2cd93d54a9dfbbadb94fa88b82062d9ae473b5b5
Brad <bradtaniguchi@gmail.com>
2024-01-07 14:20:00 -0800

    fix: playwright same aria-label issue (#219)
    * fix: playwright same aria-label issue
    
    * fix: local run issues with playwright

--- apps/internal-client-e2e/playwright.config.ts
+++ apps/internal-client-e2e/playwright.config.ts
@@ -5,7 +5,14 @@ import { baseConfig } from '../../playwright.config.base';
 const config: PlaywrightTestConfig = {
   ...baseConfig,
   // lower the timeout to 5 seconds as this app should be fast.
-  timeout: 5_000,
+  timeout: 20_000,
+  reporter: 'html',
+  // always have 2 retries
+  retries: 2,
+  use: {
+    ...baseConfig.use,
+    trace: 'on',
+  },
 };
 
 if (config.use?.baseURL === 'local') {
